2774958fc97b4431b231f87a29a766cdc11050ba
Backshifted <backshifted@gmail.com>
2014-09-03 11:19:44 +0200

    Some preps for Hashes & design

--- checksum/frmMain.cs
+++ checksum/frmMain.cs
@@ -12,12 +12,24 @@ namespace checksum
 {
     public partial class frmMain : Form
     {
+        /*Notes
+         * Hash methods to add:
+         *  MD5
+         *  SHA1
+         *  SHA3
+         *  SHA256
+         *  SHA512
+         *  AES256
+        */
+        
         public frmMain()
         {
-            string[] args = Environment.GetCommandLineArgs();
-
-
             InitializeComponent();
+            cmbMethod.Items.AddRange(new string[] { "MD5", "SHA1", "SHA256", "SHA512", "SHA3", "AES256" } );
+            
+            string[] args = Environment.GetCommandLineArgs();
+            
+
         }
 
         private void frmMain_Load(object sender, EventArgs e)
