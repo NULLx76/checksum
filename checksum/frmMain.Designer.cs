9b37c0c80dbda774db24b40809f0d8fe5d7e19fe
Backshifted <backshifted@gmail.com>
2014-09-04 08:53:11 +0200

    Finished the GUI

--- checksum/frmMain.Designer.cs
+++ checksum/frmMain.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.cmbMethod = new System.Windows.Forms.ComboBox();
             this.lblMethod = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.lblChecksum1 = new System.Windows.Forms.Label();
+            this.lblChecksum2 = new System.Windows.Forms.Label();
+            this.tbChecksum1 = new System.Windows.Forms.TextBox();
+            this.tbChecksum2 = new System.Windows.Forms.TextBox();
+            this.pbCheck = new System.Windows.Forms.PictureBox();
+            this.btnFile1 = new System.Windows.Forms.Button();
+            this.btnFile2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheck)).BeginInit();
             this.SuspendLayout();
             // 
             // cmbMethod
@@ -46,7 +46,7 @@
             this.cmbMethod.FormattingEnabled = true;
             this.cmbMethod.Location = new System.Drawing.Point(64, 12);
             this.cmbMethod.Name = "cmbMethod";
-            this.cmbMethod.Size = new System.Drawing.Size(121, 21);
+            this.cmbMethod.Size = new System.Drawing.Size(223, 21);
             this.cmbMethod.TabIndex = 0;
             // 
             // lblMethod
@@ -58,76 +58,76 @@
             this.lblMethod.TabIndex = 1;
             this.lblMethod.Text = "Method:";
             // 
-            // label2
+            // lblChecksum1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(77, 108);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.lblChecksum1.AutoSize = true;
+            this.lblChecksum1.Location = new System.Drawing.Point(12, 51);
+            this.lblChecksum1.Name = "lblChecksum1";
+            this.lblChecksum1.Size = new System.Drawing.Size(69, 13);
+            this.lblChecksum1.TabIndex = 2;
+            this.lblChecksum1.Text = "Checksum 1:";
             // 
-            // label3
+            // lblChecksum2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(53, 121);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.lblChecksum2.AutoSize = true;
+            this.lblChecksum2.Location = new System.Drawing.Point(12, 77);
+            this.lblChecksum2.Name = "lblChecksum2";
+            this.lblChecksum2.Size = new System.Drawing.Size(69, 13);
+            this.lblChecksum2.TabIndex = 3;
+            this.lblChecksum2.Text = "Checksum 2:";
             // 
-            // textBox1
+            // tbChecksum1
             // 
-            this.textBox1.Location = new System.Drawing.Point(125, 118);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.tbChecksum1.Location = new System.Drawing.Point(87, 48);
+            this.tbChecksum1.Name = "tbChecksum1";
+            this.tbChecksum1.Size = new System.Drawing.Size(200, 20);
+            this.tbChecksum1.TabIndex = 4;
             // 
-            // textBox2
+            // tbChecksum2
             // 
-            this.textBox2.Location = new System.Drawing.Point(125, 141);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.tbChecksum2.Location = new System.Drawing.Point(87, 74);
+            this.tbChecksum2.Name = "tbChecksum2";
+            this.tbChecksum2.Size = new System.Drawing.Size(200, 20);
+            this.tbChecksum2.TabIndex = 5;
             // 
-            // pictureBox1
+            // pbCheck
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(293, 88);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.pbCheck.Location = new System.Drawing.Point(292, 76);
+            this.pbCheck.Name = "pbCheck";
+            this.pbCheck.Size = new System.Drawing.Size(16, 16);
+            this.pbCheck.TabIndex = 6;
+            this.pbCheck.TabStop = false;
             // 
-            // button1
+            // btnFile1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 98);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnFile1.Location = new System.Drawing.Point(314, 43);
+            this.btnFile1.Name = "btnFile1";
+            this.btnFile1.Size = new System.Drawing.Size(72, 23);
+            this.btnFile1.TabIndex = 8;
+            this.btnFile1.Text = "From File...";
+            this.btnFile1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnFile2
             // 
-            this.button2.Location = new System.Drawing.Point(125, 88);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnFile2.Location = new System.Drawing.Point(314, 72);
+            this.btnFile2.Name = "btnFile2";
+            this.btnFile2.Size = new System.Drawing.Size(72, 23);
+            this.btnFile2.TabIndex = 9;
+            this.btnFile2.Text = "From File...";
+            this.btnFile2.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(418, 209);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(398, 107);
+            this.Controls.Add(this.btnFile2);
+            this.Controls.Add(this.btnFile1);
+            this.Controls.Add(this.pbCheck);
+            this.Controls.Add(this.tbChecksum2);
+            this.Controls.Add(this.tbChecksum1);
+            this.Controls.Add(this.lblChecksum2);
+            this.Controls.Add(this.lblChecksum1);
             this.Controls.Add(this.lblMethod);
             this.Controls.Add(this.cmbMethod);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -137,7 +137,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Checksum";
             this.Load += new System.EventHandler(this.frmMain_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheck)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,13 +147,13 @@
 
         private System.Windows.Forms.ComboBox cmbMethod;
         private System.Windows.Forms.Label lblMethod;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblChecksum1;
+        private System.Windows.Forms.Label lblChecksum2;
+        private System.Windows.Forms.TextBox tbChecksum1;
+        private System.Windows.Forms.TextBox tbChecksum2;
+        private System.Windows.Forms.PictureBox pbCheck;
+        private System.Windows.Forms.Button btnFile1;
+        private System.Windows.Forms.Button btnFile2;
     }
 }
 
