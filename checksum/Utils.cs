4aab8e5614f17c24edbb4b37a20363a2fccc2c8e
Ivandro Ismael <ivandrofly@hotmail.com>
2016-03-04 01:42:15 +0000

    Move Utils method to a new file

---
+++ checksum/Utils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Checksum
+{
+    public static class Utils
+    {
+        public static void AddToContextMenu()
+        {
+            string regkey = (string)Registry.GetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum\\command", null, null);
+            string regIcon = (string)Registry.GetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum", "Icon", null);
+            if (regkey == null || !regkey.Contains(Application.ExecutablePath))
+                Registry.SetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum\\command", null, Application.ExecutablePath + " %1");
+            if (regIcon == null || regIcon != Application.ExecutablePath)
+                Registry.SetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum", "Icon", Application.ExecutablePath);
+        }
+
+        public static void RemoveFromContextMenu()
+        {
+            var checkSumKey = Registry.ClassesRoot.OpenSubKey("*\\shell\\", true);
+            checkSumKey.DeleteSubKeyTree("Checksum");
+            checkSumKey.Close();
+        }
+
+        public static string CalculateMD5Hash(string file)
+        {
+            using (var md5 = MD5.Create())
+            using (var stream = File.OpenRead(file))
+            {
+                byte[] b = md5.ComputeHash(stream);
+                return BitConverter.ToString(b).Replace("-", string.Empty).ToLowerInvariant();
+            }
+        }
+
+        public static string CalculateSHA1Hash(string file)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            using (var stream = File.OpenRead(file))
+            {
+                byte[] b = sha1.ComputeHash(stream);
+                return BitConverter.ToString(b).Replace("-", string.Empty).ToLowerInvariant();
+            }
+        }
+
+        public static string CalculateSHA256Hash(string file)
+        {
+            using (var sha256 = SHA256.Create())
+            using (var stream = File.OpenRead(file))
+            {
+                byte[] b = sha256.ComputeHash(stream);
+                return BitConverter.ToString(b).Replace("-", string.Empty).ToLowerInvariant();
+            }
+        }
+
+        public static string CalculateSHA512Hash(string file)
+        {
+            using (var sha512 = SHA512.Create())
+            using (var stream = File.OpenRead(file))
+            {
+                byte[] b = sha512.ComputeHash(stream);
+                return BitConverter.ToString(b).Replace("-", string.Empty).ToLowerInvariant();
+            }
+        }
+    }
+}
--- checksum/frmMain.cs
+++ checksum/frmMain.cs
@@ -88,60 +88,6 @@ namespace checksum
 
         #region "Functions"
 
-        public void checkRegistry()
-        {
-            string regkey = (string)Registry.GetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum\\command", null, null);
-            string regIcon = (string)Registry.GetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum", "Icon", null);
-            if (regkey == null || !regkey.Contains(Application.ExecutablePath))
-                Registry.SetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum\\command", null, Application.ExecutablePath + " %1");
-            if (regIcon == null || regIcon != Application.ExecutablePath)
-                Registry.SetValue("HKEY_CLASSES_ROOT\\*\\shell\\Checksum", "Icon", Application.ExecutablePath);
-        }
-
-        public string CalculateMD5Hash(string file)
-        {
-            using (var md5 = MD5.Create())
-            using (var stream = File.OpenRead(file))
-            {
-                byte[] b = md5.ComputeHash(stream);
-                stream.Close();
-                return BitConverter.ToString(b).Replace("-", "").ToLower();
-            }
-        }
-
-        public string CalculateSHA1Hash(string file)
-        {
-            using (SHA1 sha1 = SHA1.Create())
-            using (var stream = File.OpenRead(file))
-            {
-                byte[] b = sha1.ComputeHash(stream);
-                stream.Close();
-                return BitConverter.ToString(b).Replace("-", "").ToLower();
-            }
-        }
-
-        public string CalculateSHA256Hash(string file)
-        {
-            using (var sha256 = SHA256.Create())
-            using (var stream = File.OpenRead(file))
-            {
-                byte[] b = sha256.ComputeHash(stream);
-                stream.Close();
-                return BitConverter.ToString(b).Replace("-", "").ToLower();
-            }
-        }
-
-        public string CalculateSHA512Hash(string file)
-        {
-            using (var sha512 = SHA512.Create())
-            using (var stream = File.OpenRead(file))
-            {
-                byte[] b = sha512.ComputeHash(stream);
-                stream.Close();
-                return BitConverter.ToString(b).Replace("-", "").ToLower();
-            }
-        }
-
         public void StartHashing(string file, HashAlgorithms method, int tb)
         {
             Text = "Checksum - Calculating Hash...";
@@ -163,16 +109,16 @@ namespace checksum
             switch (method)
             {
                 case HashAlgorithms.SHA1:
-                    output = CalculateSHA1Hash(file);
+                    output = Utils.CalculateSHA1Hash(file);
                     break;
                 case HashAlgorithms.SHA256:
-                    output = CalculateSHA256Hash(file);
+                    output = Utils.CalculateSHA256Hash(file);
                     break;
                 case HashAlgorithms.SHA512:
-                    output = CalculateSHA512Hash(file);
+                    output = Utils.CalculateSHA512Hash(file);
                     break;
                 case HashAlgorithms.MD5:
-                    output = CalculateMD5Hash(file);
+                    output = Utils.CalculateMD5Hash(file);
                     break;
             }
             EnableForm();
